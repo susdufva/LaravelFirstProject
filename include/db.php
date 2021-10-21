55aaa5108728e9bc38e86564083e34a1b514fbc7
dufva <sussiedufva@hotmail.com>
2021-10-21 15:55:45 +0200

    getting started

---
+++ include/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=wedslyoal";
+$user = "root";
+$password = "";
+
+$pdo = new PDO($dsn, $user, $password); 
+
+?>
---
+++ views/confirm.php
@@ -0,0 +1,21 @@
+<?php
+    include("../include/db.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="../css/homepage.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Lobster&display=swap" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <div>
+        </div>
+    </main>
+</body>
+</html>
