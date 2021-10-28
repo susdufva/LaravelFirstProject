27f85cf116dedc38d5ab8b29b40de42a669fb7b8
dufva <sussiedufva@hotmail.com>
2021-10-28 15:51:40 +0200

    database connection working and step 2/4 designed

--- first-project/resources/views/couples/step2.blade.php
+++ first-project/resources/views/couples/step2.blade.php
@@ -1,8 +1,16 @@
 @extends('base')
 @section('main')
 <div class="row">
-    <div class="col-sm-8 offset-sm-2">
-        <h1 class="display-3">When and where</h1>
+    <div class="col">
+        <div class="step">
+            <p class="step1">Step 2/4 </p> 
+        </div>
+        <div class="h1"> 
+            <h1 class="display">When & where</h1>
+        </div>
+        <div class="span"> 
+            <p class="names"> A joyous wedding starts with really great planning. We are happy to help</br> you get there. </p>
+        </div>
         <div>
             @if($errors->any())
             <div class="alert alert-danger">
@@ -16,17 +24,25 @@
             <form method="post" action="{{ route('couples.store') }}">
                 @csrf
                 <div class="form-group">
-                    <label for="location">Location</label>
-                    <input type="search" class="form-control" name="location"/>
+                    <label class="label3" for="location">Somewhere around here</label>
+                    <input type="search" class="input3" name="location"/>
                 </div>
+          
                 <div class="form-group">
-                    <label for="date">Date</label>
-                    <input type="date" class="form-control" id="date" name="date">
+                    <label class="label4" for="date">Probably on this date</label>
+                    <input type="date" class="input4" name="date"/>
                 </div>
-               
-                <button type="submit" class="btn btn-primary-outline">Continue</button>
-                <a href="step1">Back</a>
-                <a href="step3">Next</a>
+        
+                <div class="span"> 
+                    <p class="change"> These details are easily changed in settings.</p>
+                </div>
+                <button type="submit" class="btn">Next</button>
+                <div class="">
+                    <a href="step3" class="cancel" id="a">Back</a>
+                    <p class="cancel" id="b"> | </p>
+                    <a href="#" class="cancel" id="c">Cancel</a>
+                </div>
+                
             </form>
         </div>
     </div>
